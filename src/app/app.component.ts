b6e058c686043990bae79f649e4cba3c9a2c07fa
n-townsend <132301374+n-townsend@users.noreply.github.com>
2023-11-14 14:31:09 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+/**
+ * @title Basic Inputs
+ * @order 1
+ * @component Input, MatFormField, MatIcon, MatCard, MatButton, MatList, MatToolbar, MatTooltip
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myFlix-Angular-app';
+  title = 'myFlix-Angular-client';
+
 }
---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss']
+})
+export class UserRegistrationFormComponent implements OnInit {
+
+  @Input() userData = { username: '', password: '', email: '', birthday: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  // This is the function responsible for sending the form inputs to the backend
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+      this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open('user registered successfully!', 'OK', {
+        duration: 2000
+      });
+
+      // log user in and navigate to movies
+      this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+        localStorage.setItem('user', JSON.stringify(result.user));
+        localStorage.setItem('token', result.token);
+        this.router.navigate(['movies']);
+      })
+    }, (response) => {
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+}
